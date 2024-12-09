920ada88a53b76e054f71060f00888aa0e6b36ed
Tomoya Honda <tomoya.honda1219@gmail.com>
2024-12-09 05:37:39 +0000

    Add day04 (Part 2)

--- 2024/04/day04.go
+++ 2024/04/day04.go
@@ -32,7 +32,7 @@ func isFoundInDirection(
 	startRow,
 	startCol int,
 	direction [2]int,
-) (isFound bool) {
+) bool {
 	numRows := len(texts)
 	numCols := len(texts[0])
 
@@ -83,6 +83,41 @@ func countWord(texts []string, word string) (wordCount int) {
 	return wordCount
 }
 
+func isFoundCrossShapeMas(texts []string, row, col int) bool {
+	numRows := len(texts)
+	numCols := len(texts[0])
+	if !(1 <= row && row < numRows-1 && 1 <= col && col < numCols-1) {
+		return false
+	}
+
+	if texts[row][col] != 'A' {
+		return false
+	}
+
+	isMasPair := func(x, y byte) bool {
+		return (x == 'M' && y == 'S') || (x == 'S' && y == 'M')
+	}
+	topLeft := texts[row-1][col-1]
+	bottomRight := texts[row+1][col+1]
+	bottomLeft := texts[row+1][col-1]
+	topRight := texts[row-1][col+1]
+	return isMasPair(topLeft, bottomRight) && isMasPair(bottomLeft, topRight)
+}
+
+func countCrossShapeMas(texts []string) (crossShapeMasCount int) {
+	numRows := len(texts)
+	numCols := len(texts[0])
+
+	for row := 0; row < numRows; row++ {
+		for col := 0; col < numCols; col++ {
+			if isFoundCrossShapeMas(texts, row, col) {
+				crossShapeMasCount++
+			}
+		}
+	}
+	return crossShapeMasCount
+}
+
 func main() {
 	// const filename = "day04.example" // size: 10x10
 	const filename = "day04.input" // size: 140x140
@@ -95,4 +130,6 @@ func main() {
 	fmt.Printf("number of \"%s\": %d\n", word, wordCount)
 
 	// Part 2
+	crossShapeMasCount := countCrossShapeMas(texts)
+	fmt.Printf("number of \"X-MAS\": %d\n", crossShapeMasCount)
 }
